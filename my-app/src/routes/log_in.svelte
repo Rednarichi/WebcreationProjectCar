757051a95d9e7268aeb07e22d0f2de57800d9c6d
saivinay9854 <ramasaivinay123@gmail.com>
2022-06-18 18:40:45 +0200

    integration of login&signup

--- my-app/src/routes/log_in.svelte
+++ my-app/src/routes/log_in.svelte
@@ -1,13 +1,43 @@
 <script>
-    import {supabase} from "../supabase.js";
-    import "../app.css";
-    import Auth from "../Auth.svelte"
-    import SignUp from "../Signup.svelte"
-    console.log(supabase);
+	import {supabase} from "..//supabase";
+    import LoginNew from "../LoginNew.svelte";
+	import Signup from "../Signup.svelte";
+	let currentTab = "Login";
+
+	const changeTab = (tab) => {
+		currentTab = tab;
+	};
 </script>
 
-<div class="container mx-auto my-6 max-wl-g">
-    <Auth/>
-    <SignUp/>
-    <slot></slot>
-</div>
+<main class="container">
+	<div class="row flex flex-center">
+		<div class="col-4 offset-4 mt-5">
+			<div class="row">
+				<div class="col tab-heading {currentTab == 'Login' ? 'tab-active': ''}" on:click={() => changeTab("Login")}>
+					<span>Login</span>
+				</div>
+				<div class="col tab-heading {currentTab == 'Signup' ? 'tab-active': ''}" on:click={() => changeTab("Signup")}>
+					<span>Create Account</span>
+				</div>
+			</div>
+			{#if currentTab === "Login"}
+				<LoginNew />
+			{:else}
+				<Signup />
+			{/if}
+		</div>
+	</div>
+</main>
+
+<style>
+	.tab-heading {
+		font-weight: bold;
+		padding: 1rem;
+		background-color: lightgray;
+	}
+
+	.tab-active {
+		background: black;
+		color: white;
+	}
+</style>
