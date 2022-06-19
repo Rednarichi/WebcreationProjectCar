2ee193cba6e791db23e80020401f59fabf26a3fd
AxelRaynal <ax.raynal@gmail.com>
2022-06-19 17:40:41 +0200

    About us update

---
+++ my-app/src/routes/about/index.svelte
@@ -0,0 +1,44 @@
+<script>
+import { space } from "svelte/internal";
+
+</script>
+<head>
+	<title>Basic Web Page</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1" >
+	<link href="style.css" rel="stylesheet">
+  </head>
+  <header>
+	</header>
+  <body>
+	<div>
+	<h1 class="text-white first-letter:text-center text-4xl font-medium uppercase">Since 2022</h1>
+	<p class="text-white text-justify pr-10 pl-10 mt-3">Let us tell you a story, a beautiful adventure that began in 2022 at essec, with four car enthusiasts at the helm. In order for a desire to become a viable project, it must be carried by people who are both invested and anchored in the field concerned. This is the case for the founders of Auto Prestige who, from their passion, were able to extract the nectar to offer it to their customers.</p>
+	<h2 class="text-white pl-10 mb-2 mt-4 text-2xl font-medium capitalize">Our history</h2>
+	<p class="text-white text-justify pr-10 pl-10">In 2022, Lucas, Reda, Vinay and Axel discover during a website creation that they have a common passion: luxury cars. 
+  That's when they had the idea to develop a car rental concept with the desire to create a company where luxury and adrenaline would be at the center of the activity. And it's not by chance that the four partners started this business. It is in fact an obvious choice: to create a company whose DNA would reflect the passion that drives them.
+  Together, they federate a whole team of professionals from various backgrounds with a single common thread: to offer the best possible service on earth.
+  </p>
+  <p class="text-white text-justify pr-10 pl-10 mb-2 mt-2"> Auto Prestige is committed to providing its customers with a customized solution, personalized as desired. The aim is to respond to a wide range of problems, with reactivity and imagination, while placing the world of luxury as an obvious solution. It must be said that the founders of the brand are themselves aware of the prestige transport that they particularly like.
+  They focus on the best in the field to meet the most demanding requirements of their customers. Their knowledge of the sector makes them particularly attentive to the evolution of the ranges, which enables them to have a rental fleet at the cutting edge of innovation.
+  They are thus able to guarantee a 5-star service whatever the situation they have to face.
+  </p>
+  <p class="text-white pr-10 text-justify pl-10">Together, the fourth partners have a wide enough range of skills to give Auto Prestige the impetus to innovate every day. Today, in 2025, the founders of Auto Prestige are delighted to see how their initial passion has grown and has allowed them to solve many travel problems.
+  
+  Beyond pure custom service, the team listens attentively to requests to find the prestigious solution that will meet the need. Thus, the company satisfies various profiles; from the customer who needs a comfortable solution on board a sedan with driver to the adrenaline enthusiast who dreams of a break at the wheel of a Lamborghini: everything is possible.
+  
+ This is the essence of Auto Prestige: to offer each client a relevant solution according to his initial problem or why not leave his comfort zone by trying out new sensations?
+  </p>
+  
+	<script src="script.js"></script>
+	  <div class='image'>
+		  </div>
+		</div>
+  </body>
+
+<style>
+	body {
+  color: rgb(0, 0, 0);
+  font-family: sans-serif;
+}
+</style>
+
